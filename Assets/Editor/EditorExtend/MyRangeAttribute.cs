b0ebac8a4b441b8b565ddaa435553548ba334a84
WanLittle <2621996061@qq.com>
2020-07-22 10:24:40 +0800

    编辑器扩展实践

---
+++ Assets/Editor/EditorExtend/MyRangeAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MyRangeAttribute : PropertyAttribute
+{
+    readonly public float min;
+    readonly public float max;
+
+    MyRangeAttribute(float min, float max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}
---
+++ Assets/Scripts/EditorExtend/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public enum IngredientUnit { Spoon, Cup, Bowl, Piece }
+
+// 自定义 Serializable 类
+[Serializable]
+public class Ingredient
+{
+    public string name;
+    public int amount = 1;
+    public IngredientUnit unit;
+}
+
+public class Recipe : MonoBehaviour
+{
+    // 在 Inspector 中将此浮点数显示为 0 到 10 之间的滑动条
+    [Range(0f, 10f)]
+    public int myFloat = 0;
+
+    public Ingredient potionResult;
+    public Ingredient[] potionIngredients;
+}
